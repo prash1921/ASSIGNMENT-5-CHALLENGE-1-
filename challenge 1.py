2d823a3db3d55a9f26334d5246dff7304c92c7f6
prash1921 <120668760+prash1921@users.noreply.github.com>
2023-01-18 12:27:31 +0530

    Add files via upload

---
+++ challenge 1.py
@@ -0,0 +1,13 @@
+class point:
+ def __init__(self):
+  self.x = x
+  self.y = y
+  self.z = z
+def doublesquaresum(self):
+   doublesquaresum(double num1,double num2,double num3)
+doublesum = 0
+returnsum
+S=doublesquaresum
+S.X
+S.Y
+S.Z
